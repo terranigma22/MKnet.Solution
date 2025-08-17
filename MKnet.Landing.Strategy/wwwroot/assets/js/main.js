a8f398202264c75641a6e88b4c27ad655ed9b2bf
Alexis Amaro Placeres <alexisamaroplaceres@gmail.com>
2025-08-17 16:17:20 -0400

    update strategy

--- MKnet.Landing.Strategy/wwwroot/assets/js/main.js
+++ MKnet.Landing.Strategy/wwwroot/assets/js/main.js
@@ -53,29 +53,6 @@ window.mobileMenuToggle = {
         });
         document.addEventListener('scroll', navmenuScrollspy);
 
-        /**
-        * Hide mobile nav on same-page/hash links
-        */
-        document.querySelectorAll('#navmenu a').forEach(navmenu => {
-            navmenu.addEventListener('click', () => {
-                if (document.querySelector('.mobile-nav-active')) {
-                    mobileNavToogle();
-                }
-            });
-
-        });
-
-        /**
-       * Toggle mobile nav dropdowns
-       */
-        document.querySelectorAll('.navmenu .toggle-dropdown').forEach(navmenu => {
-            navmenu.addEventListener('click', function (e) {
-                e.preventDefault();
-                this.parentNode.classList.toggle('active');
-                this.parentNode.nextElementSibling.classList.toggle('dropdown-active');
-                e.stopImmediatePropagation();
-            });
-        });
     }
 };
 
