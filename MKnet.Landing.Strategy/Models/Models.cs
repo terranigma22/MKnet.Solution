b7f554cf2f19f57a0251c0c408fb8bced66c0ca6
Alexis Amaro Placeres <alexisamaroplaceres@gmail.com>
2025-08-20 11:18:37 -0400

    update members

--- MKnet.Landing.Strategy/Models/Models.cs
+++ MKnet.Landing.Strategy/Models/Models.cs
@@ -1,4 +1,6 @@
 ﻿namespace MKnet.Landing.Strategy.Models
 {
     public record ServiceDTO(string Title, string SubTitle, string Image, string Description);
+
+    public record MemberDTO(string Name, string SubTitle, string Phone, string Image, string Description, string DiscordUrl);
 }
