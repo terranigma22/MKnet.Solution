f0aefc0f15f0ee91c1a512a594f5ff3fa3b16df6
Alexis Amaro Placeres <alexisamaroplaceres@gmail.com>
2025-08-20 16:36:39 -0400

    update sections

--- MKnet.Landing.Strategy/Domain/Models/Models.cs
+++ MKnet.Landing.Strategy/Domain/Models/Models.cs
@@ -2,7 +2,13 @@
 {
     public record ServiceDTO(string Title, string SubTitle, string Image, string Description);
 
-    public record MemberDTO(string Name, string SubTitle, string Phone, string Image, string Description, string DiscordUrl);
+    public record MemberDTO(string Name, string Position, string Image, string Description, string WhatsappUrl, string FacebookUrl, string DiscordUrl);
 
-    public record FaqDTO(string Title, string Description);
+    public record FaqDTO(string Question, string Answer);
+
+    public record AboutDTO(string Title, string Image = "about.webp", string Description = "");
+    public record HeroDTO(string Title, string Image = "hero.webp", string Description = "");
+    public record FooterDTO(string Title, string Description = "");
+
+    public record SectionDTO(string Title, string Link);
 }
---
+++ MKnet.Landing.Strategy/Domain/Services/JsonHelper.cs
@@ -0,0 +1,39 @@
+﻿using System.Text.Json;
+
+namespace MKnet.Landing.Strategy.Domain.Services
+{
+    public static class JsonHelper
+    {
+        public static async Task<List<T>> LoadList<T>(string filename)
+        {
+            try
+            {
+                string fileName = $"Data/{filename}.json";
+                using FileStream openStream = File.OpenRead(fileName);
+                List<T>? result = await JsonSerializer.DeserializeAsync<List<T>>(openStream);
+
+                return result is null ? new List<T>() : result;
+            }
+            catch (Exception)
+            {
+                return new List<T>();
+            }
+        }
+
+        public static async Task<T?> Load<T>(string filename)
+        {
+            try
+            {
+                string fileName = $"Data/{filename}.json";
+                using FileStream openStream = File.OpenRead(fileName);
+                T? result = await JsonSerializer.DeserializeAsync<T>(openStream);
+
+                return result;
+            }
+            catch (Exception)
+            {
+                return default;
+            }
+        }
+    }
+}
