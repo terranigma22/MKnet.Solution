2f6fa980aff022f35660e4b7be73e98fa412e2ce
Alexis Amaro Placeres <alexisamaroplaceres@gmail.com>
2025-08-20 17:22:04 -0400

    update media

--- MKnet.Landing.Strategy/Domain/Models/Models.cs
+++ MKnet.Landing.Strategy/Domain/Models/Models.cs
@@ -1,14 +1,11 @@
 ﻿namespace MKnet.Landing.Strategy.Domain.Models
 {
     public record ServiceDTO(string Title, string SubTitle, string Image, string Description);
-
     public record MemberDTO(string Name, string Position, string Image, string Description, string WhatsappUrl, string FacebookUrl, string DiscordUrl);
-
     public record FaqDTO(string Question, string Answer);
-
-    public record AboutDTO(string Title, string Image = "about.webp", string Description = "");
-    public record HeroDTO(string Title, string Image = "hero.webp", string Description = "");
-    public record FooterDTO(string Title, string Description = "");
-
-    public record SectionDTO(string Title, string Link);
+    public record AboutDTO(string Title = "", string Image = "about.webp", string Description = "");
+    public record HeroDTO(string Title= "", string Image = "hero.webp", string Description = "");
+    public record FooterDTO(string Title = "", string Description = "");
+    public record SectionDTO(string Title = "", string Link = "");
+    public record MediaDTO(string Title = "", string Category = "", string Image = "media.png");
 }
